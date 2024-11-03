d20dfe3dce769be86bae86a965cefc536c85f0dd
KnightMiner <knightminer4@gmail.com>
2024-11-03 02:59:59 -0500

    Adjust sizes for permalink icons based on header size
    Now that I'm using H4 headers, found 1.25 was too big. But did not want to make it smaller, so made it conditional on the element type

--- assets/js/slimeknights.js
+++ assets/js/slimeknights.js
@@ -8,10 +8,12 @@ $(function() {
     $(':header').each(function() {
         var id = $(this).attr('id');
         if (typeof(id) === 'undefined') return;
+        var tagName = $(this).prop('tagName')
+        var size = tagName < "H3" ? 1.5 : tagName == "H3" ? 1.25 : 1;
 
         $(this)
             .addClass('hover-container')
-            .append('<a href="#' + id + '" class="uk-margin-small-left hover-icon" title="Permalink"><i class="uk-icon" uk-icon="icon: link; ratio: 1.25"></i></a>')
-            .append('<a href="#" class="uk-float-right uk-margin-right hover-icon" title="Top of Page"><i class="uk-icon" uk-icon="icon: chevron-up; ratio: 1.5"></i></a>');
+            .append(`<a href="#${id}" class="uk-margin-small-left hover-icon" title="Permalink"><i class="uk-icon" uk-icon="icon: link; ratio: ${size}"></i></a>`)
+            .append(`<a href="#" class="uk-float-right uk-margin-right hover-icon" title="Top of Page"><i class="uk-icon" uk-icon="icon: chevron-up; ratio: ${size}"></i></a>`);
     });
 });
