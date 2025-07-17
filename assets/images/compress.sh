454dc54576f8211a35b79096da52a09e4e891361
paypur <pieceofpaypur@gmail.com>
2025-07-17 08:26:58 -0700

    Compressed all images
    Took 1 hour 9 minutes

---
+++ assets/images/compress.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+compress() {
+  tmpfile=$(mktemp --suffix=.png)
+  magick "$1" -colors 256 PNG8:"$tmpfile"
+  mv "$tmpfile" "$1"
+}
+
+shopt -s globstar
+for img in **/*.png; do
+  colors=$(magick "$img" -format %k info:)
+  if magick identify -verbose "$img" | grep -q "Class: DirectClass" && [ "$colors" -lt 256 ]; then
+    printf "Compressing: %s\n" "$img"
+    compress "$img" &
+  fi
+done
+
