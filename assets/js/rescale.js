faa6647c14f44884a22260916c8113c8f6c0dbb9
paypur <pieceofpaypur@gmail.com>
2025-07-22 10:53:58 -0700

    Added buttons between book pages
    Took 1 hour 3 minutes

--- assets/js/rescale.js
+++ assets/js/rescale.js
@@ -2,10 +2,7 @@ const page = document.querySelector(".post");
 const content = document.querySelector(".book-page-html");
 
 function scale(page, content) {
-    const pageWidth = page.clientWidth;
-    const contentWidth = content.scrollWidth;
-    const scale = Math.min(1, pageWidth / contentWidth);
-    content.style.transform = `scale(${scale})`;
+    content.style.transform = `scale(${Math.min(1, page.clientWidth / content.scrollWidth)})`;
 }
 
 scale(page, content);
