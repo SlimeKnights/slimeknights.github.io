10843cbdf172394744d8bd62e1acd153a09d7105
paypur <pieceofpaypur@gmail.com>
2025-07-24 17:19:34 -0700

    Recompressed images correctly hopefully
    Took 3 hours 5 minutes

--- assets/images/compress.sh
+++ assets/images/compress.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-compress() {
-  tmpfile=$(mktemp --suffix=.png)
-  magick "$1" -colors 256 PNG8:"$tmpfile"
-  mv "$tmpfile" "$1"
-}
-
-shopt -s globstar
-for img in **/*.png; do
+# shellcheck disable=SC2016
+find "assets/images/book/tinkers/" -name "*.png" -type f -print0 | xargs -0 -n1 -P16 bash -c '
+  img="$0"
   colors=$(magick "$img" -format %k info:)
+  tmpfile=$(mktemp --suffix=.png)
   if magick identify -verbose "$img" | grep -q "Class: DirectClass" && [ "$colors" -lt 256 ]; then
+    printf "Indexing and Compressing: %s (%d colors)\n" "$img" "$colors"
+    magick "$img" -strip -dither None -colors 256 PNG8:"$tmpfile"
+    pngcrush -rem allb -brute "$tmpfile" "$img" > /dev/null 2>&1
+  else
     printf "Compressing: %s (%d colors)\n" "$img" "$colors"
-    compress "$img" &
+    pngcrush -rem allb -brute "$img" "$tmpfile" > /dev/null 2>&1
+    mv "$tmpfile" "$img"
   fi
-done
-
+'
