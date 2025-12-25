8b1f4f3a5e140dbbae46023ce3757e0438df7185
paypur <pieceofpaypur@gmail.com>
2025-12-25 14:48:10 -0800

    Fixed prefetching for different book versions

--- assets/js/book-html.js
+++ assets/js/book-html.js
@@ -5,11 +5,22 @@ const scale = (page, content) => {
     content.style.transform = `scale(${Math.min(1, page.clientWidth / content.scrollWidth)})`;
 }
 scale(page, content);
-// eliminates resize flashes
+// eliminates initial resize flashes
 content.style.visibility = "visible";
 window.addEventListener("resize", () => scale(page, content));
 
-// prefetch HTML and image
+
+const img_link = content.querySelector("img").src;
+
+let i = img_link.length - 1;
+for (let counter = 0; counter < 2 && i > 0; i--) {
+    if (img_link.charAt(i) === '/') counter++;
+}
+
+const path_prefix = img_link.substring(0, i+1);
+
+
+/* prefetch HTML and img for links hovered */
 const prefetch = anchor => {
     if (!anchor.href) return;
     const listener = () => {
@@ -24,7 +35,7 @@ const prefetch = anchor => {
         const image = parts[parts.length - 2].replace("page", "clean").replaceAll("-", "_");
 
         const link_img = document.createElement('link');
-        link_img.href = `/assets/images/book/tinkers/3.10.2/${book}/${image}.png`;
+        link_img.href = `${path_prefix}/${book}/${image}.png`;
         link_img.rel = "prefetch";
         link_img.type = "image";
         document.head.appendChild(link_img);
